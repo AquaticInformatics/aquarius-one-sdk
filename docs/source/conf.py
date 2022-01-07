fb8272ea52240c3a02dbde8c62dbda4dadbcbb59
Steve Wood <swood@hach.com>
2022-01-07 13:19:07 -0700

    TryStorming Documentation with Sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Aquarius.ONE.SDK'
+copyright = '2022, Aquatic Informatics'
+author = 'Aquatic Informatics'
 
 release = '0.1'
 version = '0.1.0'
