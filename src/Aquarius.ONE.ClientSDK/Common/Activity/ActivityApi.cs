0582addab8950e10b30cfcb357f302f65c331c3f
Dan Gossner <dan.gossner@aquaticinformatics.com>
2022-05-23 15:17:58 -0500

    Completed adding support for Activity api

--- src/Aquarius.ONE.ClientSDK/Common/Activity/ActivityApi.cs
+++ src/Aquarius.ONE.ClientSDK/Common/Activity/ActivityApi.cs
@@ -4,164 +4,186 @@ using ONE.Utilities;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using System.Linq;
+using System.Net;
+using System.Text;
 using ONE.Models.CSharp;
-
+using Proto = ONE.Models.CSharp;
 
 namespace ONE.Common.Activity
-
 {
     public class ActivityApi
     {
+        private readonly PlatformEnvironment _environment;
+        private readonly bool _continueOnCapturedContext;
+        private readonly RestHelper _restHelper;
+        public event EventHandler<ClientApiLoggerEventArgs> Event = delegate { };
+
         public ActivityApi(PlatformEnvironment environment, bool continueOnCapturedContext, RestHelper restHelper)
         {
             _environment = environment;
             _continueOnCapturedContext = continueOnCapturedContext;
             _restHelper = restHelper;
         }
-        private PlatformEnvironment _environment;
-        private bool _continueOnCapturedContext;
-        private RestHelper _restHelper;
-        public event EventHandler<ClientApiLoggerEventArgs> Event = delegate { };
 
-        
-        public async Task<List<ONE.Models.CSharp.Activity>> GetActivitiesAsync(string twinRefId, string createdById, DateTime startDate, DateTime endDate, string scheduleTypeId)
+        public async Task<List<Proto.Activity>> GetActivitiesAsync(string authTwinRefId = null, bool? includeActivityDescendants = null, bool? includeAuthTwinDescendants = null, string activityTypeId = null,
+            int? statusCode = null, int? priorityCode = null, DateTime? startDate = null, DateTime? endDate = null, string scheduleId = null, string context = null)
         {
             var watch = System.Diagnostics.Stopwatch.StartNew();
 
-            var requestId = Guid.NewGuid();
-            string sDate = startDate.ToString("MM/dd/yyyy HH:mm:ss");
-            string eDate = endDate.ToString("MM/dd/yyyy HH:mm:ss");
-            List<ONE.Models.CSharp.Activity> activities = new List<ONE.Models.CSharp.Activity>();
             try
             {
-                var respContent = await _restHelper.GetRestProtocolBufferAsync(requestId, $"/common/schedule/v1/schedule?twinRefId={twinRefId}&createdById={createdById}&startDate={sDate}&endDate={eDate}&scheduleTypeId={scheduleTypeId}").ConfigureAwait(_continueOnCapturedContext);
-                if (respContent.ResponseMessage.IsSuccessStatusCode)
-                {
-                    foreach (var activity in respContent.ApiResponse.Content.Activities.Items)
-                        activities.Add(activity);
-                    
-                    Event(null, new ClientApiLoggerEventArgs { EventLevel = EnumEventLevel.Trace, HttpStatusCode = respContent.ResponseMessage.StatusCode, ElapsedMs = watch.ElapsedMilliseconds, Module = "ActivityApi", Message = $"GetActivitiesAsync Success" });
-                    return activities;
-                }
-                else
-                {
-                    Event(null, new ClientApiLoggerEventArgs { EventLevel = EnumEventLevel.Warn, HttpStatusCode = respContent.ResponseMessage.StatusCode, ElapsedMs = watch.ElapsedMilliseconds, Module = "ActivityApi", Message = $"GetActivitiesAsync Failed" });
-                    return null;
-                }
+                var queryParameters = new Dictionary<string, string>();
+
+                if (!string.IsNullOrEmpty(authTwinRefId))
+                    queryParameters.Add(nameof(authTwinRefId), authTwinRefId);
+
+                if (includeActivityDescendants.HasValue)
+                    queryParameters.Add(nameof(includeActivityDescendants), includeActivityDescendants.Value.ToString());
+
+                if (includeAuthTwinDescendants.HasValue)
+                    queryParameters.Add(nameof(includeAuthTwinDescendants), includeAuthTwinDescendants.Value.ToString());
+
+                if (!string.IsNullOrEmpty(activityTypeId))
+                    queryParameters.Add(nameof(activityTypeId), activityTypeId);
+
+                if (statusCode.HasValue)
+                    queryParameters.Add(nameof(statusCode), statusCode.Value.ToString());
+
+                if (priorityCode.HasValue)
+                    queryParameters.Add(nameof(priorityCode), priorityCode.Value.ToString());
+
+                if (startDate.HasValue)
+                    queryParameters.Add(nameof(startDate), startDate.Value.ToString("O"));
+
+                if (endDate.HasValue)
+                    queryParameters.Add(nameof(endDate), endDate.Value.ToString("O"));
+
+                if (!string.IsNullOrEmpty(scheduleId))
+                    queryParameters.Add(nameof(scheduleId), scheduleId);
+
+                if (!string.IsNullOrEmpty(context))
+                    queryParameters.Add(nameof(context), context);
+
+                var sb = new StringBuilder("/common/activity/v1");
+
+                if (queryParameters.Any())
+                    sb.Append("?");
+
+                foreach (var queryParameter in queryParameters)
+                    sb.Append($"{queryParameter.Key}={queryParameter.Value}&");
+
+                var respContent = await _restHelper.GetRestProtocolBufferAsync(Guid.NewGuid(), sb.ToString().TrimEnd('&')).ConfigureAwait(_continueOnCapturedContext);
+
+                Event(null,
+                    respContent.ResponseMessage.IsSuccessStatusCode
+                        ? CreateLoggerArgs(EnumEventLevel.Trace, "GetActivitiesAsync Success", respContent.ResponseMessage.StatusCode, watch.ElapsedMilliseconds)
+                        : CreateLoggerArgs(EnumEventLevel.Warn, "GetActivitiesAsync Failed", respContent.ResponseMessage.StatusCode, watch.ElapsedMilliseconds));
+
+                return respContent.ResponseMessage.IsSuccessStatusCode ? respContent.ApiResponse.Content.Activities.Items.ToList() : null;
             }
             catch (Exception e)
             {
-                Event(e, new ClientApiLoggerEventArgs { EventLevel = EnumEventLevel.Error, Module = "ActivityApi", Message = $"GetActivitiesAsync Failed - {e.Message}" });
+                Event(e, CreateLoggerArgs(EnumEventLevel.Error, $"GetActivitiesAsync Failed - {e.Message}"));
                 throw;
             }
         }
-        public async Task<ONE.Models.CSharp.Activity> GetOneActivityAsync(string activityId, bool includeDescendants = false)
+
+        public async Task<List<Proto.Activity>> GetOneActivityAsync(string activityId, bool includeDescendants = false)
         {
             var watch = System.Diagnostics.Stopwatch.StartNew();
 
-            var requestId = Guid.NewGuid();
-
-            List<HistorianData> historianData = new List<HistorianData>();
             try
             {
-                var respContent = await _restHelper.GetRestProtocolBufferAsync(requestId, $"/common/activity/v1/{activityId}?includeDescendants={includeDescendants}").ConfigureAwait(_continueOnCapturedContext);
-                if (respContent.ResponseMessage.IsSuccessStatusCode)
-                {
-                    foreach (var activity in respContent.ApiResponse.Content.Activities.Items)
-                    {
-                        Event(null, new ClientApiLoggerEventArgs { EventLevel = EnumEventLevel.Trace, HttpStatusCode = respContent.ResponseMessage.StatusCode, ElapsedMs = watch.ElapsedMilliseconds, Module = "ActivityApi", Message = $"GetOneActivityAsync Success" });
-                        return activity;
-                    }
-                }
-                Event(null, new ClientApiLoggerEventArgs { EventLevel = EnumEventLevel.Warn, HttpStatusCode = respContent.ResponseMessage.StatusCode, ElapsedMs = watch.ElapsedMilliseconds, Module = "ActivityApi", Message = $"GetOneActivityAsync Failed" });
-                return null;
+                var respContent = await _restHelper.GetRestProtocolBufferAsync(Guid.NewGuid(), $"/common/activity/v1/{activityId}?includeDescendants={includeDescendants}")
+                    .ConfigureAwait(_continueOnCapturedContext);
+
+                Event(null,
+                    respContent.ResponseMessage.IsSuccessStatusCode
+                        ? CreateLoggerArgs(EnumEventLevel.Trace, "GetOneActivityAsync Success", respContent.ResponseMessage.StatusCode, watch.ElapsedMilliseconds)
+                        : CreateLoggerArgs(EnumEventLevel.Warn, "GetOneActivityAsync Failed", respContent.ResponseMessage.StatusCode, watch.ElapsedMilliseconds));
+
+                return respContent.ResponseMessage.IsSuccessStatusCode ? respContent.ApiResponse.Content.Activities.Items.ToList() : null;
             }
             catch (Exception e)
             {
-                Event(e, new ClientApiLoggerEventArgs { EventLevel = EnumEventLevel.Error, Module = "ActivityApi", Message = $"GetOneActivityAsync Failed - {e.Message}" });
+                Event(e, CreateLoggerArgs(EnumEventLevel.Error, $"GetOneActivityAsync Failed - {e.Message}"));
                 throw;
             }
         }
-        public async Task<bool> SaveActivityAsync(ONE.Models.CSharp.Activity activity)
-        {
-            var watch = System.Diagnostics.Stopwatch.StartNew();
 
-            var requestId = Guid.NewGuid();
-            var endpoint = $"/common/activity/v1/";
-            if (activity == null)
+        public async Task<bool> SaveActivitiesAsync(Activities activities)
+        {
+            if (activities == null || !activities.Items.Any())
                 return true;
-            var json = JsonConvert.SerializeObject(activity, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
-            try
-            {
-                var respContent = await _restHelper.PostRestJSONAsync(requestId, json, endpoint).ConfigureAwait(_continueOnCapturedContext);
-                if (respContent.ResponseMessage.IsSuccessStatusCode)
-                {
-                    Event(null, new ClientApiLoggerEventArgs { EventLevel = EnumEventLevel.Trace, HttpStatusCode = respContent.ResponseMessage.StatusCode, ElapsedMs = watch.ElapsedMilliseconds, Module = "ActivityApi", Message = $"SaveActivityAsync Success" });
-                    return true;
-                }
-                else
-                {
-                    Event(null, new ClientApiLoggerEventArgs { EventLevel = EnumEventLevel.Warn, HttpStatusCode = respContent.ResponseMessage.StatusCode, ElapsedMs = watch.ElapsedMilliseconds, Module = "ActivityApi", Message = $"SaveActivityAsync Failed" });
-                    return false;
-                }
-
-            }
-            catch (Exception e)
-            {
-                Event(e, new ClientApiLoggerEventArgs { EventLevel = EnumEventLevel.Error, Module = "ActivityApi", Message = $"SaveActivityAsync Failed - {e.Message}" });
-                throw;
-            }
-        }
-        public async Task<bool> UpdateActivityAsync(ONE.Models.CSharp.Activity activity)
-        {
-            var watch = System.Diagnostics.Stopwatch.StartNew();
-
-            var requestId = Guid.NewGuid();
-            var endpoint = $"/common/activity/v1/";
-            if (activity == null)
-                return true;
-            var json = JsonConvert.SerializeObject(activity, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
-            try
-            {
-                var respContent = await _restHelper.PutRestJSONAsync(requestId, json, endpoint).ConfigureAwait(_continueOnCapturedContext);
-                if (respContent.ResponseMessage.IsSuccessStatusCode)
-                {
-                    Event(null, new ClientApiLoggerEventArgs { EventLevel = EnumEventLevel.Trace, HttpStatusCode = respContent.ResponseMessage.StatusCode, ElapsedMs = watch.ElapsedMilliseconds, Module = "ActivityApi", Message = $"UpdateActivityAsync Success" });
-                    return true;
-                }
-                else
-                {
-                    Event(null, new ClientApiLoggerEventArgs { EventLevel = EnumEventLevel.Warn, HttpStatusCode = respContent.ResponseMessage.StatusCode, ElapsedMs = watch.ElapsedMilliseconds, Module = "ActivityApi", Message = $"UpdateActivityAsync Failed" });
-                    return false;
-                }
-
-            }
-            catch (Exception e)
-            {
-                Event(e, new ClientApiLoggerEventArgs { EventLevel = EnumEventLevel.Error, Module = "ActivityApi", Message = $"UpdateActivityAsync Failed - {e.Message}" });
-                throw;
-            }
-        }
-        public async Task<bool> DeleteActivityAsync(string activityId, bool includeDescendants = false)
-        {
 
             var watch = System.Diagnostics.Stopwatch.StartNew();
-            var requestId = Guid.NewGuid();
+
             try
             {
-                var respContent = await _restHelper.DeleteRestJSONAsync(requestId, $"/common/activity/v1/{activityId}?includeDescendants={includeDescendants}").ConfigureAwait(_continueOnCapturedContext);
-                if (respContent.ResponseMessage.IsSuccessStatusCode)
-                    Event(null, new ClientApiLoggerEventArgs { EventLevel = EnumEventLevel.Trace, HttpStatusCode = respContent.ResponseMessage.StatusCode, ElapsedMs = watch.ElapsedMilliseconds, Module = "ActivityApi", Message = $"DeleteActivityAsync Success" });
-                else
-                    Event(null, new ClientApiLoggerEventArgs { EventLevel = EnumEventLevel.Warn, HttpStatusCode = respContent.ResponseMessage.StatusCode, ElapsedMs = watch.ElapsedMilliseconds, Module = "ActivityApi", Message = $"DeleteActivityAsync Failed" });
+                var json = JsonConvert.SerializeObject(activities, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+                var respContent = await _restHelper.PostRestJSONAsync(Guid.NewGuid(), json, "/common/activity/v1/").ConfigureAwait(_continueOnCapturedContext);
+
+                Event(null,
+                    respContent.ResponseMessage.IsSuccessStatusCode
+                        ? CreateLoggerArgs(EnumEventLevel.Trace, "SaveActivitiesAsync Success", respContent.ResponseMessage.StatusCode, watch.ElapsedMilliseconds)
+                        : CreateLoggerArgs(EnumEventLevel.Warn, "SaveActivitiesAsync Failed", respContent.ResponseMessage.StatusCode, watch.ElapsedMilliseconds));
                 return respContent.ResponseMessage.IsSuccessStatusCode;
             }
             catch (Exception e)
             {
-                Event(e, new ClientApiLoggerEventArgs { EventLevel = EnumEventLevel.Error, Module = "ActivityApi", Message = $"DeleteActivityAsync Failed - {e.Message}" });
+                Event(e, CreateLoggerArgs(EnumEventLevel.Error, $"SaveActivitiesAsync Failed - {e.Message}"));
                 throw;
             }
         }
-       
+
+        public async Task<bool> UpdateActivitiesAsync(Activities activities)
+        {
+            if (activities == null || !activities.Items.Any())
+                return true;
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            try
+            {
+                var json = JsonConvert.SerializeObject(activities, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+                var respContent = await _restHelper.PutRestJSONAsync(Guid.NewGuid(), json, "/common/activity/v1/").ConfigureAwait(_continueOnCapturedContext);
+
+                Event(null,
+                    respContent.ResponseMessage.IsSuccessStatusCode
+                        ? CreateLoggerArgs(EnumEventLevel.Trace, "UpdateActivitiesAsync Success", respContent.ResponseMessage.StatusCode, watch.ElapsedMilliseconds)
+                        : CreateLoggerArgs(EnumEventLevel.Warn, "UpdateActivitiesAsync Failed", respContent.ResponseMessage.StatusCode, watch.ElapsedMilliseconds));
+                return respContent.ResponseMessage.IsSuccessStatusCode;
+            }
+            catch (Exception e)
+            {
+                Event(e, CreateLoggerArgs(EnumEventLevel.Error, $"UpdateActivitiesAsync Failed - {e.Message}"));
+                throw;
+            }
+        }
+
+        public async Task<bool> DeleteActivityAsync(string activityId, bool includeDescendants = true)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            try
+            {
+                var respContent = await _restHelper.DeleteRestJSONAsync(Guid.NewGuid(), $"/common/activity/v1/{activityId}?includeDescendants={includeDescendants}")
+                    .ConfigureAwait(_continueOnCapturedContext);
+
+                Event(null,
+                    respContent.ResponseMessage.IsSuccessStatusCode
+                        ? CreateLoggerArgs(EnumEventLevel.Trace, "DeleteActivityAsync Success", respContent.ResponseMessage.StatusCode, watch.ElapsedMilliseconds)
+                        : CreateLoggerArgs(EnumEventLevel.Warn, "DeleteActivityAsync Failed", respContent.ResponseMessage.StatusCode, watch.ElapsedMilliseconds));
+                return respContent.ResponseMessage.IsSuccessStatusCode;
+            }
+            catch (Exception e)
+            {
+                Event(e, CreateLoggerArgs(EnumEventLevel.Error, $"DeleteActivityAsync Failed - {e.Message}"));
+                throw;
+            }
+        }
+
+        private ClientApiLoggerEventArgs CreateLoggerArgs(EnumEventLevel level, string message, HttpStatusCode statusCode = default, long duration = default) => new ClientApiLoggerEventArgs
+            { EventLevel = level, HttpStatusCode = statusCode, ElapsedMs = duration, Module = "ActivityApi", Message = message };
     }
 }
