00bba0fde6a66fe5f6b3629e994a3793582e141d
Dan Gossner <dan.gossner@aquaticinformatics.com>
2022-08-10 17:08:11 -0500

    Adding LogbookCache

--- src/Aquarius.ONE.ClientSDK/CacheHelper.cs
+++ src/Aquarius.ONE.ClientSDK/CacheHelper.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using ONE;
-using ONE.Operations;
+﻿using ONE.Operations;
 
 namespace ONE
 {
@@ -10,12 +6,14 @@ namespace ONE
     {
         ClientSDK _clientSDK;
         public ONE.Common.Library.Cache LibaryCache { get; set; }
+        public Common.Logbook.LogbookCache LogbookCache { get; set; }
         public OperationsCache OperationsCache { get; set; }
         public ONE.Enterprise.Report.Cache ReportCache { get; set; }
         public CacheHelper(ClientSDK clientSDK)
         {
             _clientSDK = clientSDK;
             LibaryCache = new Common.Library.Cache(clientSDK);
+            LogbookCache = new Common.Logbook.LogbookCache(clientSDK);
             OperationsCache = new OperationsCache(clientSDK);
             ReportCache = new ONE.Enterprise.Report.Cache();
 
---
+++ src/Aquarius.ONE.ClientSDK/Common/Logbook/LogbookCache.cs
@@ -0,0 +1,110 @@
+﻿using Newtonsoft.Json;
+using ONE.Models.CSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ONE.Utilities;
+using Proto = ONE.Models.CSharp;
+
+namespace ONE.Common.Logbook
+{
+    public class LogbookCache
+    {
+        private readonly ClientSDK _clientSdk;
+        
+        public LogbookCache(ClientSDK clientSdk)
+        {
+            _clientSdk = clientSdk;
+        }
+
+        public Dictionary<string, Proto.Configuration> Logbooks { get; set; }
+        public Dictionary<string, List<string>> Tags { get; set; }
+        public Dictionary<string, List<ConfigurationNote>> LogbookEntries { get; set; }
+
+        /// <summary>
+        /// Load Logbook and logbookEntry data for an operation, by default it will load entries for the last thirty days but that can be overridden
+        /// </summary>
+        /// <param name="operationId">Identifier of the operation for which to load data</param>
+        /// <param name="dataTimeSpan"></param>
+        public async Task<bool> LoadAsync(string operationId, TimeSpan dataTimeSpan = default)
+        {
+            try
+            {
+                Logbooks = (await _clientSdk.Logbook.GetLogbooksAsync(operationId)).ToDictionary(k => k.Id, v => v);
+
+                var endDate = DateTime.UtcNow;
+                var startDate = endDate.Subtract(dataTimeSpan == default ? TimeSpan.FromDays(30) : dataTimeSpan);
+
+                foreach (var logbookId in Logbooks.Keys)
+                {
+                    LogbookEntries[logbookId] = await _clientSdk.Logbook.GetLogbookEntriesAsync(logbookId, startDate, endDate);
+
+                    if (!Tags.ContainsKey(logbookId))
+                        Tags[logbookId] = new List<string>();
+
+                    foreach (var entry in LogbookEntries[logbookId])
+                        Tags[logbookId].AddRange(entry.Tags.Select(t => t.Tag));
+
+                    Tags[logbookId] = Tags[logbookId].Distinct().ToList();
+                }
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public Proto.Configuration GetLogbook(string logbookId)
+        {
+            if (string.IsNullOrEmpty(logbookId) || !Logbooks.ContainsKey(logbookId))
+                return null;
+
+            return Logbooks[logbookId];
+        }
+
+        public List<Proto.Configuration> GetLogbooks() => Logbooks.Values.ToList();
+
+        // Latest
+        public Dictionary<string, ConfigurationNote> GetLatestEntries() =>
+            LogbookEntries.ToDictionary(k => k.Key, v => v.Value.OrderByDescending(n => n.NoteTime.ToDateTime()).First());
+
+        // Tags
+        public List<string> GetUniqueTags(string logbookId)
+        {
+            if (string.IsNullOrEmpty(logbookId) || !LogbookEntries.ContainsKey(logbookId))
+                return null;
+
+            return Tags[logbookId];
+        }
+
+        // Entries
+        
+
+        public override string ToString()
+        {
+            try
+            {
+                return JsonConvert.SerializeObject(this, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+            }
+            catch
+            {
+                return base.ToString();
+            }
+        }
+
+        public static LogbookCache Load(string serializedObject)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<LogbookCache>(serializedObject, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
