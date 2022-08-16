2f61b9534b6a9759f7b9f12907d47ab55f6fe364
Steve Wood <swood@hach.com>
2022-08-16 13:19:51 -0600

    3.2.1 Added Report Caching to Operation Cache

--- src/Aquarius.ONE.ClientSDK/CacheHelper.cs
+++ src/Aquarius.ONE.ClientSDK/CacheHelper.cs
@@ -1,24 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using ONE;
-using ONE.Operations;
+﻿using ONE.Operations;
 
 namespace ONE
 {
     public class CacheHelper
     {
-        ClientSDK _clientSDK;
-        public ONE.Common.Library.Cache LibaryCache { get; set; }
+        public Common.Library.Cache LibaryCache { get; set; }
         public OperationsCache OperationsCache { get; set; }
-        public ONE.Enterprise.Report.Cache ReportCache { get; set; }
         public CacheHelper(ClientSDK clientSDK)
         {
-            _clientSDK = clientSDK;
             LibaryCache = new Common.Library.Cache(clientSDK);
             OperationsCache = new OperationsCache(clientSDK);
-            ReportCache = new ONE.Enterprise.Report.Cache();
-
         }
     }
 }
--- src/Aquarius.ONE.ClientSDK/Enterprise/Report/Cache.cs
+++ src/Aquarius.ONE.ClientSDK/Enterprise/Report/Cache.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using ONE.Models.CSharp;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ONE.Enterprise.Report
 {
     public class Cache
     {
-        public DateTime ReportStartDate { get; set; }
-        public DateTime ReportEndDate { get; set; }
+        private string _operationId;
+        private ClientSDK _clientSDK { get; set; }
+        public Cache(ClientSDK clientSDK, string operationId)
+        {
+            _clientSDK = clientSDK;
+            _operationId = operationId;
+        }
+        public List<ReportDefinition> ReportDefinitions { get; set; }
+        public bool IsCached { get; set; }
+
+        public async Task<bool> LoadAsync()
+        {
+            if (IsCached)
+                return true;
+
+            try
+            {
+                ReportDefinitions = await _clientSDK.Report.GetDefinitionsAsync(_operationId);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
